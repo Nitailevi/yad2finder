227d7a66b69bb34e7fcf0356a4f7803272093066
Nitai levi <nitailevi@gmail.com>
2025-08-31 16:57:00 +0300

    Update notify_telegram.py

--- notify_telegram.py
+++ notify_telegram.py
@@ -1,7 +1,7 @@
 import requests
 
-TELEGRAM_TOKEN = "8472128023:AAF2aUHNFhpTAtH6BGLLV0D7vXva6nfSF2I"   # never share this
-TELEGRAM_CHAT_ID = "6926892944"          # number from @userinfobot
+TELEGRAM_TOKEN = ""   # never share this
+TELEGRAM_CHAT_ID = ""          # number from @userinfobot
 
 def send_telegram(title: str, body: str):
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
